ec5309749947bae9a374b6cfd1932ff2daaadd20
Ondairos <ondairos@gmail.com>
2024-03-21 22:09:36 +0200

    removed user slice

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice.ts";
-import userReducer from "./user/userSlice.ts";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    user: userReducer,
   },
 });
 
