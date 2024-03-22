55db9aa66975abfe40b3bd8bc8a7e67348ef8154
Ondairos <ondairos@gmail.com>
2024-03-22 14:02:37 +0200

    error css

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const NotFound = () => {
   return (
-    <div>
+    <div className="errorPage">
       <h2>Error! Page not found!</h2>
       <Link to={"/"}>Home</Link>
     </div>
